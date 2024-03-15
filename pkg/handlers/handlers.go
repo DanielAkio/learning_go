faabaf0fece844c26db0cd46361d4428dd7188ed
Daniel Akio Teixeira <danielakioteixeira@gmail.com>
2024-03-15 09:13:13 -0300

    Adding usecache config and repository pattern

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -3,15 +3,30 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/DanielAkio/go-course/pkg/config"
 	"github.com/DanielAkio/go-course/pkg/render"
 )
 
-// Home is the home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
+var Repo *Repository
+
+type Repository struct {
+	App *config.AppConfig
+}
+
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "home.page.tmpl")
 }
 
-// About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "about.page.tmpl")
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -6,32 +6,39 @@ import (
 	"net/http"
 	"path/filepath"
 	"text/template"
+
+	"github.com/DanielAkio/go-course/pkg/config"
 )
 
+var app *config.AppConfig
+
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
+
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	tc, err := createTemplateCache()
-	if err != nil {
-		log.Fatal(err)
+	var tc map[string]*template.Template
+	if app.UseCache {
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
 	}
 
 	t, ok := tc[tmpl]
 	if !ok {
-		log.Fatal(err)
+		log.Fatal("Could not get template from template cache")
 	}
 
 	buf := new(bytes.Buffer)
-	err = t.Execute(buf, nil)
-	if err != nil {
-		log.Println(err)
-	}
+	_ = t.Execute(buf, nil)
 
-	_, err = buf.WriteTo(w)
+	_, err := buf.WriteTo(w)
 	if err != nil {
-		log.Println(err)
+		log.Println("Error writing template to browser", err)
 	}
 }
 
-func createTemplateCache() (map[string]*template.Template, error) {
+func CreateTemplateCache() (map[string]*template.Template, error) {
 	myCache := map[string]*template.Template{}
 
 	pages, err := filepath.Glob("./templates/*.page.tmpl")
