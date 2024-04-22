e6298845f274d94d749ab1af1a73e9aa8bda4497
Daniel Akio Teixeira <danielakioteixeira@gmail.com>
2024-04-22 08:23:54 -0300

    Incrementa doc dos scripts para rodar

--- run.bat
+++ run.bat
@@ -1,2 +1,5 @@
 go build -o booking.exe ./cmd/web
 booking.exe
+
+@REM Using powershell
+@REM ./run.bat
--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-go build -o booking ./cmd/web && ./booking
+go build -o booking.exe ./cmd/web && ./booking.exe
+
+# Using git bash
+# ./run.sh
